7a9cccf0fa390484732200ee8a24409a989a9a38
Christian Augustin <johnchristianaugustin@gmail.com>
2025-01-05 13:10:46 +0100

    sensitivity

--- config.h
+++ config.h
@@ -9,54 +9,61 @@ static const unsigned int default_speed = 500;
 
 /* changes the speed of the mouse pointer */
 static SpeedBinding speed_bindings[] = {
-    /* key             speed */  
-    { XK_Super_L,      3000 },
-    { XK_Alt_L,        1500 },
-    { XK_a,            100  },
-    { XK_Control_L,    10   },
+	/* key             speed */
+	{ XK_Shift_L,       2000 },
+	// { XK_Alt_L,        1500 },
+	// { XK_a,            100  },
+	{ XK_Control_L,    200   },
 };
 
 /* moves the mouse pointer
  * you can also add any other direction (e.g. diagonals) */
 static MoveBinding move_bindings[] = {
-    /* key         x      y */
-    { XK_j,        -1,     0 },
-    { XK_l,         1,     0 },
-    { XK_i,         0,    -1 },
-    { XK_k,         0,     1 },
+	/* key         x      y */
+	{	XK_h,       -1,     0 },
+	{	XK_l,        1,     0 },
+	{	XK_j,        0,     1 },
+	{	XK_k,        0,    -1 },
+	{	XK_y,       -1,     -1 },
+	{	XK_u,        1,     -1 },
+	{	XK_b,       -1,    1 },
+	{	XK_n,        1,    1 },
 };
 
 /* 1: left
  * 2: middle
  * 3: right */
 static ClickBinding click_bindings[] = {
-    /* key         button */  
-    { XK_space,    1 },
-    { XK_f,        1 },
-    { XK_d,        2 },
-    { XK_s,        3 },
+	/* key         button */
+	{ XK_space,    1 },
+	{ XK_f,        1 },
+	{ XK_d,        2 },
+	{ XK_s,        3 },
 };
 
 /* scrolls up, down, left and right
  * a higher value scrolls faster */
 static ScrollBinding scroll_bindings[] = {
-    /* key        x      y */
-    { XK_n,        0 ,    25 },
-    { XK_p,        0 ,   -25 },
-    { XK_plus,     0 ,    80 },
-    { XK_minus,    0 ,   -80 },
-    { XK_h,        25,    0  },
-    { XK_g,       -25,    0  },
+	/* key        x      y */
+	// { XK_n,        0 ,    25 },
+	// { XK_p,        0 ,   -25 },
+	// { XK_plus,     0 ,    80 },
+	// { XK_minus,    0 ,   -80 },
+	// { XK_h,        25,    0  },
+	// { XK_g,       -25,    0  },
+	// scroll down with control + d
+	{ ControlMask, XK_d, 0, 25 },
+	{ ControlMask, XK_u, 0, -25 },
 };
 
 /* executes shell commands */
 static ShellBinding shell_bindings[] = {
-    /* key         command */  
-    { XK_b,        "wmctrl -a firefox" },
-    { XK_0,        "xdotool mousemove 0 0" },
+	/* key         command */
+	// { XK_b,        "wmctrl -a firefox" },
+	{ XK_0,        "xdotool mousemove 0 0" },
 };
 
 /* exits on key release which allows click and exit with one key */
 static KeySym exit_keys[] = {
-    XK_Escape, XK_q, XK_space
+	XK_Escape, XK_q, XK_space
 };
